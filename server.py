bf550433395a87face7473413cf8144b896a75cf
12roee12 <92437000+12roee12@users.noreply.github.com>
2022-06-03 18:37:38 +0300

    הכל עובד חלק
    <3 :)

--- server.py
+++ server.py
@@ -2,7 +2,8 @@ import select
 import socket
 import hashlib  # for one-side-encryption (for passwords)
 import shutil
-from flask import session# stuff for the website to work
+
+from flask import session  # stuff for the website to work
 from Db_handler import Db_handler
 
 
@@ -30,19 +31,21 @@ class Server:
         self.open_client_sockets.append(connection)
 
     def end_connection(self, current_socket):
+        # when client wants to close connection
         self.open_client_sockets.remove(current_socket)
         current_socket.close()
         print("client left")
 
     def read_data(self, current_socket):
+        # handles one msg from client
         order = current_socket.recv(1).decode()
-        if order == "7":
-            # 7 = order for passing file from client to server.
-            # how msg looks: 7 + length_of_data + data
-            file = self.get_file_from_client(current_socket)
-            self.insert_file(file)
+        # there are no orders 7 and 8 because there were such orders but i figured i dont need them.
         if order == "9":
+            # means client wants to close connection
             self.end_connection(current_socket)
+        elif order == "5":
+            # means client wants to upload new chart to the website
+            self.handle_order_5(current_socket)
         else:
             data = current_socket.recv(self.MAX_MSG_LENGTH).decode()
             if order == "1":
@@ -53,29 +56,27 @@ class Server:
                 # 2 = client wants to sign up
                 # name_length (zfilled 2) + name + password_length (zfilled 2) + password
                 self.sign_up(data)
-            elif order == "3":
-                # 3 = client want to look on specific chart
-                # msg = chart_name
-                file_data = self.get_chart(data)
-                msg = "3" + str(len(file_data)).zfill(8) + str(file_data)
-                self.messages_to_send.append((current_socket, msg))
-                comments = self.get_comments(data)
-                msg = "4" + str(len(comments)).zfill(8) + str(comments)
-                self.messages_to_send.append((current_socket, msg))
             elif order == "4":
-                # data = chart_name_length (zfilled 2) + chart_name + name_length + name + content
+                # means client wants to write a comment to some chart
+                # data = chart_name_length (zfilled 2) + chart_name + name_length
+                # + name + content
                 self.comment(data)
-            elif order == "5":
-                # data = length_name (zfilled 2) + name + genre
-                self.upload(data)
+            elif order == "3":
+                # means client wants to get specific chart and comments
+                file_data = self.get_chart(data)
+                comments = self.get_comments(data)
+                msg = bytes("3".encode()) + bytes(str(len(file_data)).encode()).zfill(8) + file_data + \
+                      bytes(str(len(comments)).encode()).zfill(8) + bytes(comments.encode())
+                self.messages_to_send.append((current_socket, msg))
             elif order == "6":
+                # means client wants to get charts according to specific genre
                 charts = self.get_by_genre(data)
                 msg = "2" + str(len(charts)).zfill(8) + charts
                 self.messages_to_send.append((current_socket, msg))
 
-    def get_file_from_client(self, current_socket):
+    def get_file(self, current_socket):
         # data = song_name_length (zfilled 2) + song_name + file_length (zfilled 8) + file
-        # returns new file with the name of the song
+        # opens a new file with the data inside
         name_length = int(current_socket.recv(2).decode())
         name = current_socket.recv(name_length).decode()
         name = name + ".pdf"
@@ -84,11 +85,13 @@ class Server:
         while file_length > self.MAX_MSG_LENGTH:
             packet = current_socket.recv(self.MAX_MSG_LENGTH)
             data = data + packet
+            file_length = file_length - self.MAX_MSG_LENGTH
         if file_length != 0:
             data = data + current_socket.recv(file_length)
-        with open(name, "wb") as f:
-            f.write(data)
-            return f
+        file_name = 'templates\\files\\' + name
+        f = open(file_name, "wb")
+        f.write(data)
+        f.close()
 
     def read_list(self, rlist):
         for current_socket in rlist:
@@ -105,7 +108,10 @@ class Server:
         for message in self.messages_to_send:
             current_socket, data = message
             if current_socket in wlist:
-                current_socket.send(data.encode())
+                if type(data) == str:
+                    current_socket.send(data.encode())
+                else:
+                    current_socket.send(data)
             self.messages_to_send.remove(message)
 
     def run(self):
@@ -115,17 +121,12 @@ class Server:
             self.read_list(rlist)
             self.send_data(wlist)
 
-    @staticmethod
-    def insert_file(file):
-        # moves file to files folder
-        shutil.move(file, "templates\\files\\" + session["song_name"])
-
     def login(self, data):
         # handles the login.
         # data = name_length (zfilled 2) + name + password
         name_length = int(data[0:2])
-        name = data[2: name_length+2]
-        password = data[name_length+2:]
+        name = data[2: name_length + 2]
+        password = data[name_length + 2:]
         password = hashlib.md5(password.encode()).hexdigest()
         task = "SELECT * FROM customers WHERE name = %(name)s AND password = %(password)s"
         info = {'name': name, 'password': password}
@@ -140,8 +141,8 @@ class Server:
         # handles signing up
         # data = name_length (zfilled 2) + name + password_length (zfilled 2) + password
         name_length = int(data[0: 2])
-        name = data[2: name_length+2]
-        password = data[name_length+2:]
+        name = data[2: name_length + 2]
+        password = data[name_length + 2:]
         password = hashlib.md5(password.encode()).hexdigest()
         task = "INSERT INTO customers (name, password) VALUES (%(name)s, %(password)s)"
         info = {'name': name, 'password': password}
@@ -150,13 +151,15 @@ class Server:
 
     @staticmethod
     def get_chart(data):
-        # handles getting charts from server
-        file = "templates\\files\\"+data+".pdf"
-        with open(file, 'rb') as f:
-            data = f.read()
-            return str(data)
+        # handles getting charts from client
+        name = data
+        file = "templates\\files\\" + name + ".pdf"
+        f = open(file, 'rb')
+        data = f.read()
+        return data
 
-    def get_comments(self, chart_name):
+    def get_comments(self, data):
+        chart_name = data
         comments = self.db.request_get_all(f'SELECT * FROM {chart_name}')
         self.db.commit()
         comments = self.convert_list_to_string(comments)
@@ -165,21 +168,22 @@ class Server:
     def comment(self, data):
         # data = chart_name_length (zfilled 2) + chart_name + name_length + name + content
         chart_name_length = int(data[0:2])
-        chart_name = data[2: chart_name_length+2]
-        name_length = int(data[chart_name_length+2: chart_name_length+4])
-        name = data[chart_name_length+4: chart_name_length+4+name_length]
-        content = data[chart_name_length+4+name_length:]
+        chart_name = data[2: chart_name_length + 2]
+        name_length = int(data[chart_name_length + 2: chart_name_length + 4])
+        name = data[chart_name_length + 4: chart_name_length + 4 + name_length]
+        content = data[chart_name_length + 4 + name_length:]
         task = f'INSERT INTO {chart_name} (name, comment) VALUES (%(name)s, %(comment)s)'
         values = {'name': name, 'comment': content}
         self.db.request_insert(task, values)
         self.db.commit()
 
-    def upload(self, data):
-        # data = song_name_length (zfilled 2) + song_name + genre
-        song_name_length = int(data[0: 2])
-        name = data[2: song_name_length+2]
-        genre = data[: song_name_length+2]
+    def upload(self, current_socket):
+        song_name_length = int(current_socket.recv(2).decode())
+        name = current_socket.recv(song_name_length).decode()
+        genre_length = int(current_socket.recv(1).decode())
+        genre = current_socket.recv(genre_length).decode()
         name = name.replace("'", "")
+        name = name.replace('"', '')
         task = 'INSERT INTO charts (name, genre, likes) VALUES (%(name)s, %(genre)s, %(likes)s)'
         info = {'name': name, 'genre': genre, 'likes': 0}
         self.db.request_insert(task, info)
@@ -198,7 +202,12 @@ class Server:
         charts = self.convert_list_to_string(charts)
         return charts
 
+    def handle_order_5(self, current_socket):
+        self.upload(current_socket)
+        self.get_file(current_socket)
+
 
 if __name__ == "__main__":
     s = Server()
     s.run()
+
