57cfe68e4abe326b522152fda47b1ea06f41ceb1
12roee12 <92437000+12roee12@users.noreply.github.com>
2022-06-12 12:41:54 +0300

    complete

--- server.py
+++ server.py
@@ -1,84 +1,101 @@
+import os
 import select
 import socket
 import hashlib  # for one-side-encryption (for passwords)
-import shutil
 
-from flask import session  # stuff for the website to work
 from Db_handler import Db_handler
 
 
-class Server:
+class Backend_server:
 
     def __init__(self):
         self.MAX_MSG_LENGTH = 1024
-        self.SERVER_PORT = 5000
-        self.SERVER_IP = "0.0.0.0"
+        self.BACKEND_SERVER_PORT = 6000
+        self.BACKEND_SERVER_IP = "0.0.0.0"
 
-        print("setting up server..")
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_socket.bind((self.SERVER_IP, self.SERVER_PORT))
-        self.server_socket.listen()
-        print("listening for clients...")
+        print("setting up backend server..")
+        self.backend_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.backend_server_socket.bind((self.BACKEND_SERVER_IP, self.BACKEND_SERVER_PORT))
+        self.backend_server_socket.listen()
+        print("listening for frontend servers...")
         # create a list of all the connected sockets.
-        self.open_client_sockets = []
-        self.open_client_names = []
+        self.open_frontend_servers_sockets = []
+        self.open_frontend_servers_names = []
         self.messages_to_send = []
         self.db = Db_handler()
 
-    def new_client(self, current_socket):
-        connection, client_address = current_socket.accept()
-        print("New client joined!", client_address)
-        self.open_client_sockets.append(connection)
+    def new_frontend_server(self, current_socket):
+        connection, frontend_server_address = current_socket.accept()
+        print("New fronend_server joined!", frontend_server_address)
+        self.open_frontend_servers_sockets.append(connection)
 
     def end_connection(self, current_socket):
-        # when client wants to close connection
-        self.open_client_sockets.remove(current_socket)
+        # when frontend server wants to close connection
+        self.open_frontend_servers_sockets.remove(current_socket)
         current_socket.close()
-        print("client left")
+        print("frontend_server left")
 
     def read_data(self, current_socket):
-        # handles one msg from client
+        # handles one msg from frontend server
         order = current_socket.recv(1).decode()
         # there are no orders 7 and 8 because there were such orders but i figured i dont need them.
         if order == "9":
-            # means client wants to close connection
+            # means frontend server wants to close connection
             self.end_connection(current_socket)
         elif order == "5":
-            # means client wants to upload new chart to the website
+            # means frontend server wants to upload new chart to the website
             self.handle_order_5(current_socket)
+        elif order == "a":
+            genres = str(self.db.request_get_all("SELECT * FROM genres"))
+            msg = "5" + str(len(genres)).zfill(8) + genres
+            self.messages_to_send.append((current_socket, msg))
+        elif order == "7":
+            # means user wants to see the owner page
+            length = int(current_socket.recv(2).decode())
+            name = current_socket.recv(length).decode()
+            charts = str(
+                self.db.request_get_where("SELECT * FROM charts WHERE creator = (%(name)s)", {"name": name}))
+            msg = "4" + str(len(charts)).zfill(8) + charts
+            self.messages_to_send.append((current_socket, msg))
+        elif order == "8":
+            length = int(current_socket.recv(2).decode())
+            name = current_socket.recv(length).decode()
+            os.remove('templates\\files\\' + name.lower() + '.pdf')
+            delete = {"delete": name}
+            self.db.delete(delete)
+            self.db.delete_table(name)
         else:
             data = current_socket.recv(self.MAX_MSG_LENGTH).decode()
             if order == "1":
-                # 1 = client wants to log in
+                # 1 = frontend server wants to log in
                 # how msg looks: name_length (zfilled 2) + name + password_length (zfilled 2) + password
                 self.messages_to_send.append((current_socket, self.login(data)))
             elif order == "2":
-                # 2 = client wants to sign up
+                # 2 = frontend server wants to sign up
                 # name_length (zfilled 2) + name + password_length (zfilled 2) + password
                 self.sign_up(data)
             elif order == "4":
-                # means client wants to write a comment to some chart
+                # means frontend server wants to write a comment to some chart
                 # data = chart_name_length (zfilled 2) + chart_name + name_length
                 # + name + content
                 self.comment(data)
             elif order == "3":
-                # means client wants to get specific chart and comments
+                # means frontend server wants to get specific chart and comments
                 file_data = self.get_chart(data)
                 comments = self.get_comments(data)
                 msg = bytes("3".encode()) + bytes(str(len(file_data)).encode()).zfill(8) + file_data + \
                       bytes(str(len(comments)).encode()).zfill(8) + bytes(comments.encode())
                 self.messages_to_send.append((current_socket, msg))
             elif order == "6":
-                # means client wants to get charts according to specific genre
+                # means frontend server wants to get charts according to specific genre
                 charts = self.get_by_genre(data)
-                msg = "2" + str(len(charts)).zfill(8) + charts
+                genres = str(self.db.request_get_all("SELECT * FROM genres"))
+                msg = "2" + str(len(charts)).zfill(8) + charts + str(len(genres)).zfill(8) + str(genres)
                 self.messages_to_send.append((current_socket, msg))
 
-    def get_file(self, current_socket):
+    def get_file(self, name, current_socket):
         # data = song_name_length (zfilled 2) + song_name + file_length (zfilled 8) + file
         # opens a new file with the data inside
-        name_length = int(current_socket.recv(2).decode())
-        name = current_socket.recv(name_length).decode()
         name = name + ".pdf"
         file_length = int(current_socket.recv(8).decode())
         data = b''
@@ -95,8 +112,8 @@ class Server:
 
     def read_list(self, rlist):
         for current_socket in rlist:
-            if current_socket is self.server_socket:
-                self.new_client(current_socket)
+            if current_socket is self.backend_server_socket:
+                self.new_frontend_server(current_socket)
             else:
                 self.read_data(current_socket)
 
@@ -116,8 +133,8 @@ class Server:
 
     def run(self):
         while True:
-            rlist, wlist, xlist = select.select([self.server_socket] + self.open_client_sockets,
-                                                self.open_client_sockets, [])
+            rlist, wlist, xlist = select.select([self.backend_server_socket] + self.open_frontend_servers_sockets,
+                                                self.open_frontend_servers_sockets, [])
             self.read_list(rlist)
             self.send_data(wlist)
 
@@ -132,9 +149,13 @@ class Server:
         info = {'name': name, 'password': password}
         msg = self.db.request_get_where(task, info)
         self.db.commit()
-        if msg is not None:
-            return "1True"
-        else:
+        print(msg)
+        try:
+            if msg[0] is not None:
+                print("1true")
+                return "1True"
+        except:
+            print("1false")
             return "1False"
 
     def sign_up(self, data):
@@ -151,15 +172,14 @@ class Server:
 
     @staticmethod
     def get_chart(data):
-        # handles getting charts from client
+        # handles getting charts from frontend server
         name = data
         file = "templates\\files\\" + name + ".pdf"
         f = open(file, 'rb')
         data = f.read()
         return data
 
-    def get_comments(self, data):
-        chart_name = data
+    def get_comments(self, chart_name):
         comments = self.db.request_get_all(f'SELECT * FROM {chart_name}')
         self.db.commit()
         comments = self.convert_list_to_string(comments)
@@ -177,15 +197,11 @@ class Server:
         self.db.request_insert(task, values)
         self.db.commit()
 
-    def upload(self, current_socket):
-        song_name_length = int(current_socket.recv(2).decode())
-        name = current_socket.recv(song_name_length).decode()
-        genre_length = int(current_socket.recv(1).decode())
-        genre = current_socket.recv(genre_length).decode()
+    def upload(self, name, genre, creator):
         name = name.replace("'", "")
         name = name.replace('"', '')
-        task = 'INSERT INTO charts (name, genre, likes) VALUES (%(name)s, %(genre)s, %(likes)s)'
-        info = {'name': name, 'genre': genre, 'likes': 0}
+        task = 'INSERT INTO charts (name, genre, creator) VALUES (%(name)s, %(genre)s, %(creator)s)'
+        info = {'name': name, 'genre': genre, 'creator': creator}
         self.db.request_insert(task, info)
         name = name.replace(" ", "_")
         name = name.lower()
@@ -202,12 +218,34 @@ class Server:
         charts = self.convert_list_to_string(charts)
         return charts
 
+    def upload_with_add(self, name, add, creator):
+        name = name.replace("'", "")
+        name = name.replace('"', '')
+        task = 'INSERT INTO charts (name, genre, creator) VALUES (%(name)s, %(genre)s, %(creator)s)'
+        info = {'name': name, 'genre': add, 'creator': creator}
+        self.db.request_insert(task, info)
+        name = name.replace(" ", "_")
+        name = name.lower()
+        self.db.create_table(name)
+        self.db.request_insert('INSERT INTO genres (genre) VALUES (%(genre)s)', {'genre': add})
+
     def handle_order_5(self, current_socket):
-        self.upload(current_socket)
-        self.get_file(current_socket)
+        is_add = current_socket.recv(1).decode()
+        name_length = int(current_socket.recv(2).decode())
+        name = current_socket.recv(name_length).decode()
+        genre_length = int(current_socket.recv(1).decode())
+        genre = current_socket.recv(genre_length).decode()
+        creator_length = int(current_socket.recv(2).decode())
+        creator = current_socket.recv(creator_length).decode()
+        if is_add == "5":
+            self.upload(name, genre, creator)
+        else:
+            add_length = int(current_socket.recv(1).decode())
+            add = current_socket.recv(add_length).decode()
+            self.upload_with_add(name, add, creator)
+        self.get_file(name, current_socket)
 
 
 if __name__ == "__main__":
-    s = Server()
+    s = Backend_server()
     s.run()
-
